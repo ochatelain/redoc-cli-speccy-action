cfb0c1e9776a4ca9519fa29fbc2485e21a500eeb
Kai Koenig <kai@ventego-creative.co.nz>
2020-05-06 18:18:22 +1200

    Cleanup for version 1.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,11 @@
 #!/bin/sh -l
-
-echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
-
 ls -al
-
 cd /github/workspace
-
 ls -al
-
 echo "redoc-cli version: $(redoc-cli --version)"
-echo $2
-redoc-cli $2
-
+redoc-cli $1
 echo "speccy version: $(speccy --version)"
-echo $3
-speccy $3
+speccy $2
 
